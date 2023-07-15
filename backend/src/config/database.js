66c43bbcfa16b16329142e05573ed3aae879d2d4
angelobarilari <98756174+angelobarilari@users.noreply.github.com>
2023-07-15 15:20:45 -0300

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,13 +2,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-console.log(process.env.POSTGRES_HOST)
-console.log(process.env.POSTGRES_USERNAME)
-console.log(process.env.POSTGRES_PASSWORD)
-console.log(process.env.POSTGRES_DATABASE)
-console.log(process.env.POSTGRES_PORT)
-console.log(process.env.APP_PORT)
-
 module.exports = {
     dialect: 'postgres',
     host: process.env.POSTGRES_HOST,
