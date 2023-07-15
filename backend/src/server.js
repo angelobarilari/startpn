c4d9fd23b60731351b8f85e13b1654c83219b2b3
angelobarilari <98756174+angelobarilari@users.noreply.github.com>
2023-07-15 13:44:12 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,6 @@
 require('dotenv').config()
 const server = require('./app')
-server.listen(process.env.APP_PORT)
+
+app.listen(process.env.APP_PORT || 3000, () => {
+    console.log(`Server is running in port ${port}`)
+});
